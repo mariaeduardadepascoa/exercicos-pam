de372c5e2c74cd570be833ec98b6605485f0eeab
Maria Eduarda <mariapascoa356@gmail.com>
2026-04-30 12:46:14 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  const [contador, setContador] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.grupoDiv}>
+      <Text style={styles.title}>Contador = {contador}</Text>
+      
+      <TouchableOpacity style={styles.buttons} onPress = {()=> setContador(contador + 1)}>
+        <Text style={styles.titleButtons}>+ 1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttons} onPress = {()=> setContador(contador - 1)}>
+        <Text style={styles.titleButtons}>- 1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttons} onPress = {()=> setContador(contador = 0)}>
+        <Text style={styles.titleButtons}>Resetar</Text>
+      </TouchableOpacity>
+
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  grupoDiv: {
+    gap: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#232323',
+    width: 230,
+    height: 230,
+    borderRadius: 10
+  },
+
+  title: {
+    color: '#fff',
+    fontWeight: 600,
+    fontSize: 20
+  },
+
+  buttons: {
+    width: 100,
+    height: 40,
+    display: 'flex',
+    justifyContent: 'center', 
+    alignItems: 'center',
+    backgroundColor: '#ae19d7',
+    borderRadius: 10,
+  },
+
+  titleButtons: {
+    color: '#fff',
+    fontSize: 16
+  }
+});
